d0e27003867f4f456f68a1264058256a101378a6
Salchy <leandrocg524@gmail.com>
2025-05-25 00:13:20 -0300

    Funcionalidad agregar imagen desde API

---
+++ tp-API_Equipo-7B/tp-API_Equipo-7B/Controllers/ImagenController.cs
@@ -0,0 +1,22 @@
+﻿using negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using tp_API_Equipo_7B.Models;
+
+namespace tp_API_Equipo_7B.Controllers
+{
+    public class ImagenController : ApiController
+    {
+        // POST: api/Imagen
+        public void Post([FromBody] ImagenDTO imgs)
+        {
+            imagenesDatos imagenes = new imagenesDatos();
+
+            imagenes.addImages(imgs.idArticulo, imgs.imagenesURL);
+        }
+    }
+}
---
+++ tp-API_Equipo-7B/tp-API_Equipo-7B/Models/ImagenDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using dominio;
+
+namespace tp_API_Equipo_7B.Models
+{
+    public class ImagenDTO
+    {
+        public int idArticulo { get; set; }
+        public List<string> imagenesURL { get; set; }
+    }
+}
